74590eacb6dae7d5dbfff0b876ec57b8e1d3e320
Golib <fredwared@gmail.com>
2022-05-15 09:23:17 +0500

    Testing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.testclient import TestClient
 
 from routes import (auth)
 
@@ -8,4 +9,8 @@ app.version = '0.0.1'
 app.description = 'Hotel service app'
 app.redoc_url = '/docs'
 
+# Routes
 app.include_router(auth.router)
+
+# Testing
+TestClient(app)

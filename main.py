e04c7b0e675dfb5090f06ecb47bb59286a7cd0ee
Golib <fredwared@gmail.com>
2022-05-15 12:58:29 +0500

    Route prefix, description, tags

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from fastapi.testclient import TestClient
 
 from routes import (default, auth)
 
-app = FastAPI()
+app = FastAPI(prefix='/v1')
 app.title = 'Hotels API'
 app.version = '0.0.1'
 app.description = 'Hotel service app'
 app.redoc_url = '/docs'
 
 # Routes
-app.include_router(default.router)
-app.include_router(auth.router)
+common_prefix = "/api/v1.0"
+app.include_router(default.router, tags=["Default routes"], include_in_schema=False)
+app.include_router(auth.router, prefix=f"{common_prefix}/auth", tags=["Auth"])
 
 # Testing
 TestClient(app)

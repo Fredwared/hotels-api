79beb714b1668faa8d1c02c85394565babb0abea
Golib <fredwared@gmail.com>
2022-05-15 11:36:34 +0500

    Default route

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
-from routes import (auth)
+from routes import (default, auth)
 
 app = FastAPI()
 app.title = 'Hotels API'
@@ -10,6 +10,7 @@ app.description = 'Hotel service app'
 app.redoc_url = '/docs'
 
 # Routes
+app.include_router(default.router)
 app.include_router(auth.router)
 
 # Testing
